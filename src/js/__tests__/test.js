19daf4dad679506f312eaed3f122bbd0758f29a4
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-19 19:24:49 +0200

    task1 test passed

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,6 +1,18 @@
 import Validator from '../task1.js';
 
-test('testing validateUsername method from Validator class', () => {
+test('testing positive validateUsername method from Validator class', () => {
   const validator = new Validator();
-  expect(validator.validateUsername('a')).toBe(true);
+  expect(validator.validateUsername('a-12b')).toBe(true);
+});
+
+test.each([
+  '1abc',
+  'abc1',
+  '-abc',
+  'abc-',
+  '_abc',
+  'abc_',
+])('check username %s', (username) => {
+  const validator = new Validator();
+  expect(validator.validateUsername(username)).toBe(false);
 });
--- src/js/task1.js
+++ src/js/task1.js
@@ -1,10 +1,17 @@
 /** This class validates usernames */
 export default class Validator {
-  regExp = /a/;
+  checkSympols = /^[\d\w_-]+$/i;
+  checkStartEndSymbols = /^[^\d_-].*[^\d_-]$/;
+  checkNumbersAmount = /(\d{4,})/;
   /** Method checks
    * @param {string} username and
    * @return {boolean} checking result */
   validateUsername(username) {
-    return this.regExp.test(username);
+    if (this.checkSympols.test(username) &&
+    this.checkStartEndSymbols.test(username) &&
+    !this.checkNumbersAmount.test(username)) {
+      return true;
+    }
+    return false;
   };
 }
